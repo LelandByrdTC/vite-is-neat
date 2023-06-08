7a7817dd49cf82910bf9a4212f3934b51c01a642
Leland Byrd <lbyrd@truecoders.io>
2023-06-08 11:18:23 -0500

    added state and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import ChildComponent from './components/childComponent'
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [believe, setBelieve] = useState(false)
+  const [musicians, setMusicians] = useState(["Louis Armstrong", "Miles Davis", "Duke Ellington", "Charlie Parker", "John Coltrane", "Ella Fitzgerald"])
 
   return (
     <>
@@ -16,10 +20,24 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <ChildComponent message="This iS the CHILDRENS"
+      belief = {believe.toString()}
+      />
+      {/* {ChildComponent(message = "this is blah blah")} */}
+      <h1>Jazz Musicians</h1>
+      <ul>
+        {musicians.map((person, id)=> {
+          return <li key={id+person}>{person}</li>
+        })}
+      </ul>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((sum) => sum - 1)}>
           count is {count}
+        </button>
+        <button onClick={()=> 
+          setBelieve(!believe)
+        }>
+         believe is {believe ? "true" : "false"}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
