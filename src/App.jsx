7c163f3c0a5b8c48730b8fd86936ac90183b7296
Leland Byrd <lbyrd@truecoders.io>
2023-06-09 10:07:54 -0500

    added states and props examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,69 @@
-import { useState } from 'react'
+import React, { Component } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import ChildComponent from './components/childComponent'
 
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-  const [believe, setBelieve] = useState(false)
-  const [musicians, setMusicians] = useState(["Louis Armstrong", "Miles Davis", "Duke Ellington", "Charlie Parker", "John Coltrane", "Ella Fitzgerald"])
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <ChildComponent message="This iS the CHILDRENS"
-      belief = {believe.toString()}
-      />
-      {/* {ChildComponent(message = "this is blah blah")} */}
-      <h1>Jazz Musicians</h1>
-      <ul>
-        {musicians.map((person, id)=> {
-          return <li key={id+person}>{person}</li>
-        })}
-      </ul>
-      <div className="card">
-        <button onClick={() => setCount((sum) => sum - 1)}>
-          count is {count}
-        </button>
-        <button onClick={()=> 
-          setBelieve(!believe)
-        }>
-         believe is {believe ? "true" : "false"}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0,
+      believe: false,
+      musicians: ["Louis Armstrong", "Miles Davis", "Duke Ellington", "Charlie Parker", "John Coltrane", "Ella Fitzgerald"]
+    }
+    this.setCount = () => {
+      this.setState((prevState) => ({
+        count: prevState.count + 1
+      }))
+    }
+    this.setBelieve = () => {
+      this.setState((prevState) => ({
+        believe: !prevState.believe
+      }))
+    }
+  }
+  render() {
+    return (
+      <>
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <ChildComponent message="This iS the CHILDRENS"
+          belief={this.state.believe.toString()}
+        />
+        <h1>Jazz Musicians</h1>
+        <ul>
+          {this.state.musicians.map((person, id) => {
+            return <li key={id + person}>{person}</li>
+          })}
+        </ul>
+        <div className="card">
+          <button onClick={() => this.setCount((sum) => sum - 1)}>
+            count is {this.state.count}
+          </button>
+          <button onClick={() =>
+            this.setBelieve(!this.state.believe)
+          }>
+            believe is {this.state.believe ? "true" : "false"}
+          </button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </>
+    )
+  }
+
 }
 
 export default App
--- src/components/ChildComponent.jsx
+++ src/components/ChildComponent.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
+import React, { Component } from "react";
 
-export default function ChildComponent(props){
-
+export default class ChildComponent extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            message: "",
+            belief: ""
+        }
+    }
+render() {
     return (
         <div>
             <p>{props.message}</p>
             <p>{props.belief}</p>
         </div>
     )
+
+}
 }
 
